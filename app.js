39f15811908db99f42a033a21b82ad0e04562731
Utsav1o1 <utsavkarki244@gmail.com>
2025-01-09 16:29:26 +0545

    error in app js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // 1 day
         httpOnly: true, 
-        secure: process.env.NODE_ENV === "production",, 
+        secure: process.env.NODE_ENV === "production",
     }
 }));
 app.use((req, res, next) => {
