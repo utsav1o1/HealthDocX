dd3cccea7277f4f6385665c5e1048810298d3e69
Utsav Karki <utsavkarki244@gmail.com>
2025-09-21 19:33:22 +0545

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,43 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URI, {
+//             //useNewUrlParser: true,
+//             //useUnifiedTopology: true,
+            
+//         });
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+// }
+
+// module.exports = connectDB;
+
+const mongoose = require("mongoose");
+
+let isConnected;
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            //useNewUrlParser: true,
-            //useUnifiedTopology: true,
-            
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-}
+  if (isConnected) {
+    console.log("=> using existing database connection");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = db.connections[0].readyState;
+    console.log("=> new database connection");
+  } catch (err) {
+    console.error("DB Connection Error:", err.message);
+    throw err;
+  }
+};
 
 module.exports = connectDB;
