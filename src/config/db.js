ac820a2aab7800cf1cd6acbc3790ab79e4c7339b
Utsav Karki <utsavkarki244@gmail.com>
2025-09-21 19:46:43 +0545

    revert Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,39 +1,39 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI, {
-//             //useNewUrlParser: true,
-//             //useUnifiedTopology: true,
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true,
             
-//         });
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (err) {
-//         console.error(err);
-//         process.exit(1);
-//     }
-// }
-
-// module.exports = connectDB;
-
-const mongoose = require("mongoose");
-
-let cached = global.mongoose;
-
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
-async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 }
 
 module.exports = connectDB;
+
+// const mongoose = require("mongoose");
+
+// let cached = global.mongoose;
+
+// if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+// async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(process.env.MONGO_URI, {
+//       // useNewUrlParser: true,
+//       // useUnifiedTopology: true,
+//     }).then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+// module.exports = connectDB;
